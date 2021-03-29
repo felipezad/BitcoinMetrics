f8373310a07abbe605ceec862e1afae644e0e248
Felipe Zadinello <felipezad@gmail.com>
2021-03-29 14:45:02 -0300

    Setup app and internet access in manifest

---
+++ app/src/main/java/com/crypto/currency/bitcoin/CryptoBitcoinApplication.kt
@@ -0,0 +1,8 @@
+package com.crypto.currency.bitcoin
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CryptoBitcoinApplication : Application() {
+}
