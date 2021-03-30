7884464a6a84fe06db61d22eb4813d3e7ba6ba80
Felipe Zadinello <felipezad@gmail.com>
2021-03-30 20:13:02 -0300

    Create three mock graphs

--- core/di/src/main/java/com/crypto/currency/di/mapper/EpochFormatter.kt
+++ core/di/src/main/java/com/crypto/currency/di/mapper/EpochFormatter.kt
@@ -8,7 +8,7 @@ import java.time.format.DateTimeFormatter
 
 object EpochFormatter : ValueFormatter() {
 
-    private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd")
 
     fun toYearMonthDayDateFormat(epochTime: Long): String {
         return try {
--- feature/bitcoin/src/main/java/com/crypto/currency/bitcoin/BitcoinChartFragment.kt
+++ feature/bitcoin/src/main/java/com/crypto/currency/bitcoin/BitcoinChartFragment.kt
@@ -1,15 +1,18 @@
 package com.crypto.currency.bitcoin
 
 import android.util.Log
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.viewModels
 import com.crypto.currency.bitcoin.databinding.FragmentBitcoinChartBinding
 import com.crypto.currency.di.mapper.EpochFormatter
 import com.crypto.currency.model.chart.BitcoinChart
 import com.crypto.currency.model.chart.ChartTypes
 import com.crypto.currency.ui.BaseFragment
-import com.github.mikephil.charting.data.Entry
-import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.components.Legend.LegendForm
+import com.github.mikephil.charting.components.XAxis.XAxisPosition
+import com.github.mikephil.charting.data.*
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -31,19 +34,71 @@ class BitcoinChartFragment : BaseFragment<BitcoinChartViewModel, FragmentBitcoin
         //TODO Change
         Log.d("setupView", "setupView")
         mViewModel.bitcoinChartData.observe(this, { bitcoinChart: BitcoinChart ->
-            bitcoinChart.values.mapTo(mutableListOf<Entry>(), { value ->
+            bitcoinChart.values.take(5).mapTo(mutableListOf<Entry>(), { value ->
                 Entry(value.x.toFloat(), value.y.toFloat())
             }).also {
                 val lineDataset = LineDataSet(it, ChartTypes.TRANSACTIONS_PER_SECOND.name)
                 val lineData = LineData(lineDataset)
 
                 mViewBinding.lineChart.run {
+                    xAxis.position = XAxisPosition.BOTTOM
+                    xAxis.granularity = 1f // only intervals of 1 day
+                    xAxis.labelCount = 7
                     xAxis.valueFormatter = EpochFormatter
                     data = lineData
+
+                    legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+                    legend.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+                    legend.orientation = Legend.LegendOrientation.HORIZONTAL
+                    legend.setDrawInside(false)
+                    legend.form = LegendForm.SQUARE
+                    legend.formSize = 9f
+                    legend.textSize = 11f
+                    legend.xEntrySpace = 4f
+
+                    invalidate()
+                }
+            }
+            bitcoinChart.values.take(5).mapTo(mutableListOf(), { value ->
+                BarEntry(value.x.toFloat(), value.y.toFloat())
+            }).also {
+                val barDataSet = BarDataSet(it, ChartTypes.TOTAL_BITCOINS.name)
+                val barData = BarData(barDataSet)
+                barData.barWidth = 0.9f
+
+                mViewBinding.barChart.run {
+
+                    xAxis.position = XAxisPosition.BOTTOM
+                    xAxis.setDrawGridLines(false)
+                    xAxis.granularity = 1f // only intervals of 1 day
+                    xAxis.labelCount = 7
+                    xAxis.valueFormatter = EpochFormatter
+
+                    data = barData
+                    setFitBars(true)
+                    invalidate()
+                }
+            }
+            bitcoinChart.values.take(5).mapTo(mutableListOf(), { value ->
+                PieEntry(value.y.toFloat())
+            }).also {
+                val pieDataSet = PieDataSet(it, ChartTypes.TRANSACTIONS_TOTAL.name)
+                val pieData = PieData(pieDataSet)
+
+                mViewBinding.pieChart.run {
+                    animateY(1400, Easing.EaseInOutQuad)
+                    data = pieData
                     invalidate()
                 }
             }
         })
+
+        mViewBinding.bitcoinScrollView.setOnScrollChangeListener(
+            NestedScrollView.OnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+                println(
+                    "ScrollX $scrollX -> ScrollY $scrollY"
+                )
+            })
     }
 
 }
