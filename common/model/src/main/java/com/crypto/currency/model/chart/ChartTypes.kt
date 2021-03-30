2ec2502c59d076ed507c33617b1fa2a9b1e7815e
Felipe Zadinello <felipezad@gmail.com>
2021-03-30 12:10:32 -0300

    Add chart types for API

--- common/model/src/main/java/com/crypto/currency/model/chart/ChartTypes.kt
+++ common/model/src/main/java/com/crypto/currency/model/chart/ChartTypes.kt
@@ -1,5 +1,8 @@
 package com.crypto.currency.model.chart
 
 enum class ChartTypes(val graphName: String) {
-    TRANSACTIONS_PER_SECOND("transactions-per-second")
+    TRANSACTIONS_PER_SECOND("transactions-per-second"),
+    TOTAL_BITCOINS("total-bitcoins"),
+    TRANSACTIONS_TOTAL("n-transactions-total"),
+    MARKET_PRICE("market-price")
 }
