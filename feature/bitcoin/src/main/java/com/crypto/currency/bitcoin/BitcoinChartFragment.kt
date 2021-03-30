b30b61c66fbd36c7bf7ed102dc5ed46fe9081352
Felipe Zadinello <felipezad@gmail.com>
2021-03-30 17:02:34 -0300

    Setup linechart

--- feature/bitcoin/src/main/java/com/crypto/currency/bitcoin/BitcoinChartFragment.kt
+++ feature/bitcoin/src/main/java/com/crypto/currency/bitcoin/BitcoinChartFragment.kt
@@ -3,8 +3,12 @@ package com.crypto.currency.bitcoin
 import android.util.Log
 import androidx.fragment.app.viewModels
 import com.crypto.currency.bitcoin.databinding.FragmentBitcoinChartBinding
+import com.crypto.currency.model.chart.BitcoinChart
 import com.crypto.currency.model.chart.ChartTypes
 import com.crypto.currency.ui.BaseFragment
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,12 +22,27 @@ class BitcoinChartFragment : BaseFragment<BitcoinChartViewModel, FragmentBitcoin
 
     override fun startViewModel() {
         //TODO Change
-        Log.d("setupViewModel", "setupViewModel")
+        Log.d("startViewModel", "startViewModel")
         mViewModel.getCharByName(ChartTypes.TRANSACTIONS_PER_SECOND)
     }
 
     override fun setupView() {
         //TODO Change
-        Log.d("setupViewModel", "setupViewModel")
+        Log.d("setupView", "setupView")
+        mViewModel.bitcoinChartData.observe(this, { bitcoinChart: BitcoinChart ->
+            bitcoinChart.values.mapTo(mutableListOf<Entry>(), { value ->
+                Entry(value.x.toFloat(), value.y.toFloat())
+            }).also {
+                val lineDataset = LineDataSet(it, ChartTypes.TRANSACTIONS_PER_SECOND.name)
+                val lineData = LineData(lineDataset)
+
+                mViewBinding.lineChart.run {
+                    data = lineData
+                    invalidate()
+                }
+            }
+        })
+
+
     }
 }
