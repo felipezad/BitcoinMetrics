2e0ed4b322b7030553b1baab2b23b7a5755a493b
Felipe Zadinello <felipezad@gmail.com>
2021-03-30 09:38:22 -0300

    Setup gradle dependencies

--- feature/bitcoin/src/main/java/com/crypto/currency/bitcoin/BitcoinFragment.kt
+++ feature/bitcoin/src/main/java/com/crypto/currency/bitcoin/BitcoinFragment.kt
@@ -4,7 +4,9 @@ import android.util.Log
 import androidx.fragment.app.viewModels
 import com.crypto.currency.bitcoin.databinding.FragmentBitcoinBinding
 import com.crypto.currency.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class BitcoinFragment : BaseFragment<BitcoinViewModel, FragmentBitcoinBinding>() {
 
     override val mViewModel: BitcoinViewModel by viewModels()
