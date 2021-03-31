a946a43145feb6781d89b8ea20b24c1840f2385e
Felipe Zadinello <felipezad@gmail.com>
2021-03-31 18:08:00 -0300

    Create unit tests for Bitcoin Mapper

---
+++ feature/bitcoin/src/test/java/com/crypto/currency/bitcoin/BitcoinChartMapperTest.kt
@@ -0,0 +1,25 @@
+package com.crypto.currency.bitcoin
+
+import com.crypto.currency.model.chart.BitcoinChart
+import com.google.common.truth.Truth
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import org.junit.Test
+
+class BitcoinChartMapperTest {
+
+    private val mapper = BitcoinChartMapper()
+
+    @Test
+    fun `Should map response object to model object correctly`() {
+        val response = BitcoinChartFakeFactory.createFullBitcoinChartResponse()
+        val expected = BitcoinChartFakeFactory.createFullBitcoinChart()
+        val bitcoinObject = mapper.to(from = response)
+        Truth.assertThat(bitcoinObject).isInstanceOf(BitcoinChart::class.java)
+        Truth.assertThat(bitcoinObject.name).isEqualTo(expected.name)
+        Truth.assertThat(bitcoinObject.description).isEqualTo(expected.description)
+        Truth.assertThat(bitcoinObject.period).isEqualTo(expected.period)
+        Truth.assertThat(bitcoinObject.status).isEqualTo(expected.status)
+        Truth.assertThat(bitcoinObject.values.size).isEqualTo(expected.values.size)
+    }
+
+}
