6483a76aa731d64162bdb471d10b1a0b3b1c58a9
Felipe Zadinello <felipezad@gmail.com>
2021-03-31 08:16:38 -0300

    Create basic bottom navigation component

--- app/src/main/java/com/crypto/currency/MainActivity.kt
+++ app/src/main/java/com/crypto/currency/MainActivity.kt
@@ -3,7 +3,9 @@ package com.crypto.currency
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.crypto.currency.bitcoin.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = navHostFragment.navController
+        navView.setupWithNavController(navController)
     }
 }
