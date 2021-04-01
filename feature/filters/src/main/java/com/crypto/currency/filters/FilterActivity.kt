c9d3fec1c2ead8ae570bc23abaa46ad5cb856f5f
Felipe Zadinello <felipezad@gmail.com>
2021-04-01 08:42:43 -0300

    Create filter activity

---
+++ feature/filters/src/main/java/com/crypto/currency/filters/FilterActivity.kt
@@ -0,0 +1,11 @@
+package com.crypto.currency.filters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FilterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filter)
+    }
+}
